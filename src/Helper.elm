cb1b633e236b21e2983a7cc854b41f7500478475
up240312-DIMH <up240312@alumnos.upa.edu.mx>
2026-03-01 18:32:38 -0600

    Tarea5_Types_DIMH

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,51 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade result =
+    List.map
+        (\nums ->
+            if nums < 0 then
+                Pending
+
+            else if nums >= 7 then
+                Approved
+
+            else
+                Failed
+        )
+        result
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction sts =
+    case sts of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction act =
+    List.map airplaneScheduleAction act
